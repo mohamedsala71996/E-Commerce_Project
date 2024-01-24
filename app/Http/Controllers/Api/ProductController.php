34a0b1ddae8432eed8c95048f4747a5cdd6a2b65
mohamedsala71996 <mohamed.sala71996@gmail.com>
2024-01-25 01:39:29 +0200

    update

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -27,7 +27,7 @@ class ProductController extends Controller
 
     public function index(Request $request)
     {
-        return Product::filter($request->query())->get();
+        return Product::with('store:id,name','category:id,name','tags:id,name')->filter($request->query())->get();
     }
 
     /**
@@ -52,19 +52,17 @@ class ProductController extends Controller
             $data = array_merge($request->except('image', 'tags'), ['slug' => Str::slug($request->name)]);
             $imagePath = $request->file('image') ? $request->file('image')->storeAs('ProductPhotos', $data['slug'] . "Product" . $request->file('image')->getClientOriginalName(), 'public') : '';
             $product = $this->productRepository->createProduct(array_merge($data, ['image' => $imagePath]));
-
             $tags = explode(',', $request->tags);
             $tag_ids = [];
             foreach ($tags as $tag_name) {
                 $slug = Str::slug($tag_name);
-                $tag = Tag::updateOrCreate([
-                    'name' => $tag_name,
-                    'slug' => $slug
-                ]);
+                $tag = Tag::updateOrCreate([ 'name' => $tag_name,'slug' => $slug]);
                 $tag_ids[] = $tag->id;
             }
             $product->tags()->attach($tag_ids);
-            return response()->json(['message' => 'Data saved successfully', 'data' => $product], 201);
+            return response()->json(['message' => 'Data saved successfully', 'data' => $product],201, [ 
+                'location' => route('products.show',$product->id)
+            ]);
         } catch (\Exception $e) {
             return response()->json(['error' => 'Failed to save data', 'message' => $e->getMessage()], 500);
         }
@@ -72,24 +70,74 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Product $product)
     {
-        //
+        return $product->load('store:id,name','category:id,name','tags:id,name');
     }
+    // public function show( $id)
+    // {
+    //     return Product::with('store:id,name','category:id,name','tags:id,name')->findOrFail($id);
+    // }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Product $product): JsonResponse
     {
-        //
+        try {
+            $request->validate([
+                'store_id' => 'nullable|exists:stores,id',
+                'category_id' => 'nullable|exists:categories,id',
+                'name' => 'sometimes|required|string|max:255|unique:products,name,' . $product->id,
+                'description' => 'nullable|string',
+                'image' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+                'price' => 'sometimes|required|numeric|min:0',
+                'compare_price' => 'nullable|numeric|gt:price', //greater than price
+                'options' => 'nullable|json',
+                'rating' => 'numeric|min:0|max:5',
+                'featured' => 'boolean',
+                'status' => 'sometimes|required|in:active,archived,draft',
+            ]);
+            $data = array_merge($request->except('image', 'tags'), ['slug' => Str::slug($request->name)]);
+            if ($request->hasFile('image')) {
+                $this->deleteFile($product->image);
+                $imagePath = $request->file('image')->storeAs('ProductPhotos', $data['slug'] . "_Product_" . $request->file('image')->getClientOriginalName(), 'public');
+            } elseif (!$request->hasFile('image') && $product->image) {
+                $imagePath = $product->image;
+            } else {
+                $imagePath = '';
+            }
+            $product->update(array_merge($data, ['image' => $imagePath]));
+            $tags = explode(',', $request->tags);
+            $tag_ids = [];
+            $saved_tags = Tag::all();
+            foreach ($tags as $tag_name) {
+                $slug = Str::slug($tag_name);
+                $tag = $saved_tags->where('slug', $slug)->first();
+                if (!$tag) {
+                    $tag = Tag::create(['name' => $tag_name, 'slug' => $slug]);
+                }
+                $tag_ids[] = $tag->id;
+            }
+            $product->tags()->sync($tag_ids);
+            return response()->json(['message' => 'Data updated successfully', 'data' => $product], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Failed to update data', 'message' => $e->getMessage()], 500);
+        }
     }
+
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Product $product): JsonResponse
     {
-        //
+        try {
+            $product->delete();
+            $this->deleteFile($product->image);
+            return response()->json(['message' => 'Data deleted successfully'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Failed to delete data', 'message' => $e->getMessage()], 500);
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,6 @@ Route::middleware('auth','checkType')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+// require __DIR__.'/auth.php';
 require __DIR__.'/dashboard.php';
 require __DIR__.'/front.php';
